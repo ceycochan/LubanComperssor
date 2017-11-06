52a9227678aae013cd897e2df61be96085c72cad
ceycochan <gumptionchan@163.com>
2017-11-06 11:02:49 +0800

    base edition

---
+++ example/src/main/java/top/zibin/luban/example/adapter/ImageAdapter.java
@@ -0,0 +1,68 @@
+package top.zibin.luban.example.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import top.zibin.luban.example.ImageBean;
+import top.zibin.luban.example.R;
+
+//import top.zibin.luban.example.ImageBean;
+
+public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageHolder> {
+    private Context mContext;
+    private List<ImageBean> mImageList = new ArrayList<>();
+
+    public ImageAdapter(List<ImageBean> imageList) {
+        mImageList = imageList == null ? mImageList : imageList;
+    }
+
+    @Override
+    public ImageHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        return new ImageHolder(LayoutInflater.from(mContext).inflate(R.layout.item_image, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ImageHolder holder, int position) {
+        ImageBean image = mImageList.get(position);
+
+        holder.originArg.setText(image.getOriginArg());
+        holder.thumbArg.setText(image.getThumbArg());
+        Glide.with(mContext)
+                .load(image.getImage())
+                .into(holder.image);
+
+
+        Log.d("cg","图片信息:"+image.getOriginArg()+"----"+image.getThumbArg());// 获取压缩后文件参数(file & resolution)
+    }
+
+    @Override
+    public int getItemCount() {
+        return mImageList.size();
+    }
+
+    class ImageHolder extends RecyclerView.ViewHolder {
+        private TextView originArg;
+        private TextView thumbArg;
+        private ImageView image;
+
+        ImageHolder(View view) {
+            super(view);
+
+            originArg = (TextView) view.findViewById(R.id.origin_arg);
+            thumbArg = (TextView) view.findViewById(R.id.thumb_arg);
+            image = (ImageView) view.findViewById(R.id.image);
+        }
+    }
+}
